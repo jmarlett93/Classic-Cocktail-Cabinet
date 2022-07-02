94cca133d111532c886ba5e90487a758b05c6252
John Marlett <1fiyero@gmail.com>
2022-07-01 22:14:41 -0400

    US5 added theme and included scss support

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <!-- <HomePage msg="Cocktail Lounge" /> -->
-  <h1>Hello</h1>
+   <HomePage msg="Cocktail Cabinet" /> 
 </template>
 
 <script>
-// import HomePage from './components/HomePage.vue';
+ import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    // HomePage,
+     HomePage,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+
+  @import "scss/custom.scss";
+  @import "~bootstrap/scss/bootstrap.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+ // ...
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import 'bootstrap';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 
