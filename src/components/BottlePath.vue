335ed553308d9185d1cf67e3d2558098c65d3a45
John Marlett <1fiyero@gmail.com>
2022-09-20 09:10:34 -0400

    bad shit

--- src/components/BottlePath.vue
+++ src/components/BottlePath.vue
@@ -1,12 +1,29 @@
 <template>
   <h1>Bottle Path homie</h1>
+  <button type="button" @click="logShit()"></button>
+  <!--
+  <select v-model="selected" multiple @change="callSearch(selected)">
+    <option value="Option 1">Option 1</option>
+    <option v-for="(result, index) in results" v-bind:key="index">{{ result }}</option>
+    ...
+  </select>-->
 </template>
 
 <script>
-import * as data from '../assets/recipes';
-console.log(data.liquors);
 export default {
   name: 'BottlePath',
+  props: {
+    selected: String,
+    results: Array,
+  },
+  methods: {
+    logShit() {
+      console.log('shit');
+    },
+    // callSearch(value) {
+    //  return (filteredResults = search(value, data.liqours, { keySelector: (obj) => obj.name }));
+    //   },
+  },
 };
 </script>
 
